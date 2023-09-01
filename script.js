2fc7988ae4cad9d21597d21e775e333963ea6d0c
Irvan Castro <castroirvan@gmail.com>
2023-09-01 23:05:25 +0800

    Clean up code. Remove wrong hover mechanic. Fix issue of grid container resizing when dimensions are changed.

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
 const container = document.querySelector(".container");
+const grid = document.querySelector(".grid");
 
-
-for (let i = 0; i < 8; i++ ) { 
+for (let i = 0; i < 16; i++ ) { 
     const rows = document.createElement("div");
     rows.classList.add("row");
 
-    for (let j = 0; j < 8; j++) {
+    for (let j = 0; j < 16; j++) {
         const boxes = document.createElement("div");
         boxes.classList.add("box");
 
         rows.appendChild(boxes);
     }
-    container.appendChild(rows)
+    grid.appendChild(rows)
 };
-
-
-
-
