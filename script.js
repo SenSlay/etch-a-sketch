6d57113039745b343301de11daaa513c44c241fc
Irvan Castro <castroirvan@gmail.com>
2023-09-27 11:47:14 +0800

    Create function createGrid and set default value to 50. Remove default styles for a el.

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 const container = document.querySelector(".container");
 const grid = document.querySelector(".grid");
 
-for (let i = 0; i < 16; i++ ) { 
-    const rows = document.createElement("div");
-    rows.classList.add("row");
-
-    for (let j = 0; j < 16; j++) {
-        const boxes = document.createElement("div");
-        boxes.classList.add("box");
-
-        rows.appendChild(boxes);
-    }
-    grid.appendChild(rows)
+// Create grid 
+function createGrid(gridSize = 50) {
+    for (let i = 0; i < gridSize; i++ ) { 
+        const rows = document.createElement("div");
+        rows.classList.add("row");
+    
+        for (let j = 0; j < gridSize; j++) {
+            const boxes = document.createElement("div");
+            boxes.classList.add("box");
+    
+            rows.appendChild(boxes);
+        }
+        grid.appendChild(rows)
+    };
 };
+
+createGrid();
