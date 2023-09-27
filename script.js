8ef605bad6dfc7ea512e6da1066f3400dac7dfa4
Irvan Castro <castroirvan@gmail.com>
2023-09-27 11:59:39 +0800

    Create functions boxColorFunction, checkBrushType, getRandomColor. Set user-select to none for grid

--- script.js
+++ script.js
@@ -17,4 +17,71 @@ function createGrid(gridSize = 50) {
     };
 };
 
+// Give grid boxes coloring function
+function boxColorFunction() {
+    const boxes = document.querySelectorAll(".box");
+    let isMouseDown = false;
+    
+    boxes.forEach((box) => {
+      box.addEventListener("mousedown", () => {
+        isMouseDown = true;
+        box.style.backgroundColor = checkBrushType();
+      });
+    
+      box.addEventListener("mouseup", () => {
+        isMouseDown = false;
+      });
+    
+      box.addEventListener("mouseenter", () => {
+        if (isMouseDown) {
+          box.style.backgroundColor = checkBrushType();
+        }
+      });
+    
+      box.addEventListener("mouseleave", () => {
+        if (isMouseDown) {
+          box.style.backgroundColor = checkBrushType();
+        }
+      });
+    });
+};
+  
+// Random color generator
+function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+};
+
+const radioBtns = document.querySelectorAll('input[name="control-btn"]');
+const colorPicked = document.getElementById("color-picker");
+const colorBtn = document.getElementById("radio1");
+
+// Check for brush type
+function checkBrushType() {
+  var selectedBtn = "";
+
+  // Check which button is selected
+  radioBtns.forEach((btn) => {
+    if (btn.checked) {
+      selectedBtn = btn.id;
+    }
+  });
+
+  // Adjust brush color accordingly
+  if (selectedBtn == "color") {
+    return colorPicked.value;
+  
+  } else if (selectedBtn == "rainbow") {
+    return getRandomColor();
+
+  } else if (selectedBtn == "eraser") {
+    return "white";
+  };
+};
+
 createGrid();
+boxColorFunction();
